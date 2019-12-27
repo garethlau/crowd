86a33bba70ece85723bc08b6afb64ee169b4186d
Gareth <gareth.kh.lau@gmail.com>
2019-12-27 17:54:40 -0500

    email auth for signup

--- server/services/passport.js
+++ server/services/passport.js
@@ -3,6 +3,13 @@ const LocalStrategy = require("passport-local");
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
 
+const keys = require("../config/keys");
+const mailjet = require("node-mailjet").connect(
+  keys.mailjetKey,
+  keys.mailjetSecret
+);
+const crypto = require('crypto')
+
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
@@ -29,6 +36,43 @@ passport.use(
         if (user) {
           return done(null, false, { message: "Email alrady is being used." });
         } else {
+          // check if email is edu
+
+                // generate token
+                const buf = crypto.randomBytes(256);
+          // send verification email
+
+
+          const request = mailjet.post("send", { version: "v3.1" }).request({
+            Messages: [
+              {
+                From: {
+                  Email: "Gareth.kh.lau@gmail.com",
+                  Name: "Gareth"
+                },
+                To: [
+                  {
+                    Email: email,
+                    Name: firstName
+                  }
+                ],
+                Subject: "Please Verify",
+                TextPart: "Hello",
+                HTMLPart:
+                  "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!",
+                CustomID: "nice"
+              }
+            ]
+          });
+
+          request
+            .then(result => {
+              console.log(result.body);
+            })
+            .catch(err => {
+              console.log(err);
+            });
+          // create (unverified) user
           let newUser = User();
           newUser.firstName = firstName;
           newUser.lastName = lastName;
@@ -40,7 +84,6 @@ passport.use(
           newUser.resetPasswordToken = "";
           newUser.resetPasswordExpires = "";
         }
-        
       });
     }
   )
