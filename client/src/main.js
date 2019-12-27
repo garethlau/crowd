bf96c0085d758dd48e88405d85d68a8b92a94c2a
yihong <yihongliu00@gmail.com>
2019-12-27 17:54:18 -0500

    working on frontend routing

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,34 @@
 import Vue from "vue";
+import VueRouter from "vue-router";
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
+Vue.use(VueRouter);
+
+const Foo = {
+	template: "<div> foo </div>"
+}
+
+const routes = [
+	{path: "/foo", component: Foo}
+]
+
+const router = new VueRouter({
+	routes
+})
 
 new Vue({
-  render: h => h(App)
-}).$mount("#app");
+  //el: '#app',
+  	router,
+	render: h=> h(App)
+  //components: { App },
+  //template: '<App/>'
+  }
+).$mount("#app")
+//new Vue({
+//	router,
+//	render: h=> h(App)
+//}).$mount("#app");
+//new Vue({
+//  render: h => h(App)
+//}).$mount("#app");
