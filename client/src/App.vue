752e464886bf696667b0c25322f2454effce93df
Gareth <gareth.kh.lau@gmail.com>
2020-01-01 03:05:25 -0500

    improved routing

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <Navbar v-bind:theme="theme"></Navbar>
-        <router-view></router-view>
+        <router-view :key="$route.fullPath"></router-view>
     </div>
 </template>
 
