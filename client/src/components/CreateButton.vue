d4ee0da04f98b0ceaaa0709f5f16bd34cabc9a35
yihong <yihongliu00@gmail.com>
2020-01-02 00:10:37 -0500

    create resource button

---
+++ client/src/components/CreateButton.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<b-button type="is-link" onclick="createResource()">New Post</b-button>
+	</div>
+</template>
+
+<script>
+export default{
+	data(){
+		return{
+			courseCode: ""
+		}
+	},
+	methods:{
+		createResource(){
+			const router = this.$router
+			router.push("#/resource/create?courseCode="+this.courseCode);
+		}
+	}
+}
+</script>
