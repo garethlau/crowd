ec1a92e08b419543fb27c6d848ea9f7681ca7bfe
Gareth <gareth.kh.lau@gmail.com>
2020-01-03 02:26:06 -0500

    + render comments + open modal + data transfer from child to parent

---
+++ client/src/components/Comment.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        {{ comment.content }}
+        {{
+            capitalizeFirst(comment.author.firstName) +
+                ' ' +
+                capitalizeFirst(comment.author.lastName)
+        }}
+    </div>
+</template>
+
+<script>
+import stringMixin from '../mixins/stringMixin';
+export default {
+    name: 'Comment',
+    props: ['comment'],
+    mixins: [stringMixin]
+};
+</script>
