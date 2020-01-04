c3c56ab12f6a6de073fb58f7099ab828d28e81c4
Gareth <gareth.kh.lau@gmail.com>
2020-01-04 01:29:55 -0500

    - redundent route

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,5 @@ const router = express.Router();
 router.use("/api/v1/user", require("./user.js"));
 router.use("/api/v1/resource", require('./resource'));
 router.use("/api/v1/content", require("./content.js"))
-router.use("/api/v1/content/create", require("./content.js"))
 router.use("/api/v1/comment", require("./comment.js"));
 module.exports = router;
