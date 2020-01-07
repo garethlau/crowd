c1e16f5336147d1e3e88eb49070be2ed4dd11d45
Gareth <gareth.kh.lau@gmail.com>
2020-01-07 14:15:11 -0500

    use comment tree and comment card components

--- client/src/components/ResourceModal.vue
+++ client/src/components/ResourceModal.vue
@@ -1,19 +1,30 @@
 <template>
-    <div class="modal-card">
+    <div class="modal-card scrollable">
+        <section class="hero is-primary">
+            <div class="hero-body">
+                <div class="container">
+                    <h1>
+                        {{ props.courseCode }}
+                    </h1>
+                </div>
+            </div>
+        </section>
         <section class="hero">
             <div class="hero-body">
                 <div class="container">
-                    <h1 class="title">
-                        {{ props.resource.title }}
-                    </h1>
                     <h2 class="subtitle">
+                        Posted by
                         {{
                             capitalizeFirst(props.resource.author.firstName) +
                                 ' ' +
                                 capitalizeFirst(props.resource.author.lastName)
                         }}
                     </h2>
-                    <div>
+                    <h1 class="title">
+                        {{ props.resource.title }}
+                    </h1>
+
+                    <div class="subtitle">
                         {{ props.resource.content.data }}
                     </div>
                 </div>
@@ -21,29 +32,83 @@
         </section>
         <section>
             <div class="container">
-                <Comment
-                    v-for="comment in props.comments"
-                    v-bind:key="comment._id"
-                    v-bind:comment="comment"
+                <ComposeComment 
+                    :resourceId="props.resource._id"
                 />
             </div>
         </section>
 
-        <button class="button" type="button" @click="$emit('clickedClose')">
-            Close
-        </button>
+        <section>
+            <div class="container" v-if="showComments">
+                <div>
+                    <CommentTree
+                        v-for="comment in props.comments"
+                        :key="comment._id"
+                        :comment="comment"
+                        :resourceId="comment.resourceId"
+                        :offset="0"
+                    />
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 
 <script>
+/**
+
+        <button class="button" type="button" @click="$emit('clickedClose')">
+            Close
+        </button>
+ */
+
+/*
+TO close all comments
+
+          <div class="container">
+                <b-button
+                    v-if="showComments"
+                    @click="toggleComments"
+                    type="is-primary"
+                    icon-left="minus-circle"
+                >
+                    Hide Comments
+                </b-button>
+                <b-button
+                    v-else
+                    @click="toggleComments"
+                    type="is-primary"
+                    icon-left="plus-circle"
+                >
+                    Show Comments
+                </b-button>
+            </div>
+*/
+/*
+each comment has a resourceId
+nested comments dont have parentIds
+
+in each Comment.vue file, call getComments to get the children of this current comment
+*/
 import stringMixin from '../mixins/stringMixin';
-import Comment from './Comment';
+import CommentTree from './CommentTree';
+import ComposeComment from './ComposeComment';
 
 export default {
     name: 'ResourceModal',
     props: ['props'],
     mixins: [stringMixin],
-    components: { Comment },
+    components: { CommentTree, ComposeComment },
+    data() {
+        return {
+            showComments: true
+        };
+    },
+    methods: {
+        toggleComments() {
+            this.showComments = !this.showComments;
+        }
+    },
     mounted() {
         console.log(this.props);
     }
@@ -53,5 +118,8 @@ export default {
 <style lang="scss" scoped>
 .modal {
     background-color: white;
+}
+.scrollable {
+    overflow-y: scroll;
 }
 </style>
