cb09836bee1843b0c7318994858000866a84ab5a
Gareth <gareth.kh.lau@gmail.com>
2020-01-08 13:35:35 -0500

    + get vote count + upvote and downvote

--- client/src/services/CommentService.js
+++ client/src/services/CommentService.js
@@ -1,7 +1,83 @@
 import axios from 'axios';
 const base = '/api/v1/comment';
 
+const vote = (commentId, isUpvote) => {
+    return new Promise((resolve, reject) => {
+        // create url
+        const url = base + '/voting';
+        const data = {
+            commentId: commentId,
+            vote: isUpvote ? 'up' : 'dn'
+        };
+        let config = {
+            withCredentials: true,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+        // make post request
+        axios
+            .post(url, data, config)
+            .then(res => {
+                if (res.status == 200) {
+                    resolve(res);
+                }
+                // catch all other status codes and reject
+                reject('There was an error.');
+            })
+            .catch(err => {
+                if (err.response.status == 400) {
+                    // Server didn't get an ID
+                    reject('There was an error.');
+                } else if (err.response.status == 401) {
+                    reject('You must be logged in to upvote.');
+                }
+                // catch all other status codes and reject
+                reject('THere was an error.');
+            });
+    });
+};
 export default class CommentService {
+    /**
+     *
+     * @param {string} commentId
+     */
+    upvote(commentId) {
+        return vote(commentId, true);
+    }
+    /**
+     *
+     * @param {string} commentId
+     */
+    downvote(commentId) {
+        return vote(commentId, false);
+    }
+
+    getVoteCount(commentId) {
+        console.log('got ocmmentId in votecount', commentId);
+        return new Promise((resolve, reject) => {
+            let url = base + '/voting';
+            let params = {
+                commentId: commentId
+            };
+            axios
+                .get(url, { params: params })
+                .then(res => {
+                    if (res.status == 200) {
+                        console.log(res);
+                        resolve(res.data.count);
+                    }
+                    reject('There was an error.');
+                })
+                .catch(err => {
+                    if (err.response.status == 500) {
+                        reject('There was an error.');
+                    }
+                    reject('There was an error.');
+                });
+        });
+    }
+
     /**
      *
      * @param {string} resourceId
