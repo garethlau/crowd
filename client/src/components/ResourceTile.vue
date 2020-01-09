5f1aac047b00abfc1188a9a4b7a7cd40649e4a9e
Gareth <gareth.kh.lau@gmail.com>
2020-01-09 12:45:58 -0500

    - emit upvote action - emit downote action - emit favourite action + icons and styling

--- client/src/components/ResourceTile.vue
+++ client/src/components/ResourceTile.vue
@@ -1,51 +1,81 @@
 <template>
-    <div class="is-child box">
-        <article>
-            <div class="columns">
-                <div class="column is-1 vote-container">
-                    <div v-on:click="upvote" class="clickable">
-                        <b-icon pack="far" icon="caret-square-up"> </b-icon>
-                    </div>
-                    <div>
-                        0
-                    </div>
-                    <div v-on:click="downvote" class="clickable">
-                        <b-icon pack="far" icon="caret-square-down"> </b-icon>
-                    </div>
+    <div class="border">
+        <div class="columns">
+            <div class="column is-1 vote-container">
+                <div v-on:click="upvote" class="clickable">
+                    <b-icon pack="far" icon="caret-square-up"> </b-icon>
                 </div>
-                <div class="column is-8" v-on:click="launchResourceModal">
-                    <div>
-                        <p>
-                            Posted by
-                            {{
-                                capitalizeFirst(data.author.firstName) +
-                                    ' ' +
-                                    capitalizeFirst(data.author.lastName)
-                            }}
-                            <span v-if="uploadedDate == modifiedDate">
-                                {{ uploadedDate }}
-                            </span>
-                            <span v-else>
-                                {{ modifiedDate }}
-                            </span>
-                        </p>
-                    </div>
-                    <div>
-                        <p class="title">
-                            {{ data.title }}
-                        </p>
-                    </div>
+                <div>
+                    0
                 </div>
-                <div class="column is-3" v-on:click="fav">
-                    <div v-if="favourite">
-                        <b-icon pack="fas" icon="star"> </b-icon>
-                    </div>
-                    <div v-else>
-                        <b-icon pack="far" icon="star"> </b-icon>
-                    </div>
+                <div v-on:click="downvote" class="clickable">
+                    <b-icon pack="far" icon="caret-square-down"> </b-icon>
                 </div>
             </div>
-        </article>
+            <div class="column content-container">
+                <div>
+                    <p>
+                        Posted by
+                        {{
+                            capitalizeFirst(data.author.firstName) +
+                                ' ' +
+                                capitalizeFirst(data.author.lastName)
+                        }}
+                        <span v-if="uploadedDate == modifiedDate">
+                            {{ uploadedDate }}
+                        </span>
+                        <span v-else>
+                            {{ modifiedDate }}
+                        </span>
+                    </p>
+                </div>
+                <div v-on:click="launchResourceModal" class="clickable">
+                    <p class="title">
+                        {{ data.title }}
+                    </p>
+                </div>
+            </div>
+            <div class="column is-1 actions-container">
+                <div v-if="favourite" class="clickable" v-on:click="fav">
+                    <b-tooltip label="Add to favourites" position="is-left">
+                        <b-icon pack="fas" icon="star"> </b-icon>
+                    </b-tooltip>
+                </div>
+                <div v-else>
+                    <b-tooltip
+                        label="Remove from favourites"
+                        position="is-left"
+                    >
+                        <b-icon
+                            pack="far"
+                            class="clickable fade-color fade"
+                            icon="star"
+                        >
+                        </b-icon>
+                    </b-tooltip>
+                </div>
+                <div>
+                    <b-tooltip label="Report this resource" position="is-left">
+                        <b-icon
+                            pack="fas"
+                            class="clickable fade-color"
+                            icon="exclamation"
+                        >
+                        </b-icon>
+                    </b-tooltip>
+                </div>
+                <div>
+                    <b-tooltip label="Share this resource" position="is-left">
+                        <b-icon
+                            pack="fas"
+                            class="clickable fade-color"
+                            icon="share-alt"
+                        >
+                        </b-icon>
+                    </b-tooltip>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -62,21 +92,18 @@ export default {
     },
     methods: {
         upvote: function() {
-            console.log(this.data);
+            console.log('upvote resource', this.data._id);
             // console.log('Upvote');
-            this.$emit('clickedDownvote');
         },
         downvote: function() {
-            // console.log('downvote');
-            this.$emit('clickedDownvote');
+            console.log('downvote resource', this.data._id);
         },
         launchResourceModal: function() {
             // console.log('launch resource modal');
             this.$emit('clickedResource', this.data);
         },
         fav: function() {
-            // console.log('FAVV');
-            this.$emit('clickedFav');
+            console.log('fav resource', this.data._id);
         }
     },
     mounted() {
@@ -96,21 +123,43 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../styles/global.scss';
 .vote-container {
     text-align: center;
-}
-.clickable {
-    &:hover {
-        cursor: pointer;
-    }
-}
-.is-1 {
-    width: 25px;
+    width: 50px;
     padding: 0px;
-    padding-top: 10px;
+    padding-top: 5px;
+    background-color: #cecece;
+    border-radius: 7px 0px 0px 7px;
 }
+.actions-container {
+    padding-top: 2px;
+    padding-bottom: 0px;
+    text-align: right;
+}
+.content-container {
+    padding-left: 20px;
+}
+.fade-color {
+    &:hover {
+        color: $primary;
+    }
+    -o-transition: color 0.2s ease-out, background 1s ease-in;
+    -ms-transition: color 0.2s ease-out, background 1s ease-in;
+    -moz-transition: color 0.2s ease-out, background 1s ease-in;
+    -webkit-transition: color 0.2s ease-out, background 1s ease-in;
+    /* ...and now override with proper CSS property */
+    transition: color 0.2s ease-out, background 1s ease-in;
+}
+
 .tile {
     margin-top: 15px;
     margin-bottom: 15px;
+}
+.columns {
+    border-style: solid;
+    border-width: 1px;
+    margin-bottom: 40px;
+    border-radius: 7px;
 }
 </style>
