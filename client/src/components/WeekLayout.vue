376b6a1bb8704c6d4a09e0bba2d1a7b23f7bd41e
Gareth <gareth.kh.lau@gmail.com>
2020-01-09 15:27:07 -0500

    + clear query paramters when modal closes + listen to escape key to close the modal

--- client/src/components/WeekLayout.vue
+++ client/src/components/WeekLayout.vue
@@ -1,31 +1,28 @@
 <template>
     <div>
         <div>
-          
-           
-        <div v-if="resources.length == 0">
-            No resources here : I dont like that this shows up no matter what.
-            Look into possibly adding a delay or loading animation while when
-            filtering the resources.
-        </div>
-        <div v-else>
-            <ResourceTile
-                v-for="resource in resources"
-                v-bind:key="resource.id"
-                v-bind:data="resource"
-                @clickedResource="launchResourceModal"
-            />
-        </div>
-               <b-loading :is-full-page="false" :active="!loaded">
-            </b-loading>
+            <div v-if="resources.length == 0">
+                No resources here : I dont like that this shows up no matter
+                what. Look into possibly adding a delay or loading animation
+                while when filtering the resources.
+            </div>
+            <div v-else>
+                <ResourceTile
+                    v-for="resource in resources"
+                    v-bind:key="resource.id"
+                    v-bind:data="resource"
+                    @clickedResource="launchResourceModal"
+                />
+            </div>
+            <b-loading :is-full-page="false" :active="!loaded"> </b-loading>
         </div>
 
         <section>
             <b-modal
                 :active.sync="isModalActive"
                 has-modal-card
+                can-cancel="[]"
                 full-screen
-                :can-cancel="['escape', 'x', 'outside']"
                 scroll="keep"
             >
                 <ResourceModal
@@ -52,7 +49,7 @@ export default {
             isModalActive: false,
             resourceModalProps: {},
             resources: null,
-            loaded: false,
+            loaded: false
         };
     },
     methods: {
@@ -66,6 +63,8 @@ export default {
             this.isModalActive = false;
             // clear resource modal props
             this.resourceModalProps = {};
+            // clear query pararmters if any
+            this.$router.push(this.$route.path);
         }
     },
     mounted() {
@@ -82,6 +81,15 @@ export default {
                 this.toast(err, 'is-danger', 3000);
                 this.loaded = true;
             });
+    },
+    created() {
+        // add event listener for escape key press
+        window.addEventListener('keydown', e => {
+            if (e.key == 'Escape') {
+                console.log('escape pressed, closing');
+                this.closeModal();
+            }
+        });
     }
     // #A8A8A8
 };
