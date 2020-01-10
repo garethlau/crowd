5125df0e7dc4f756fa2a91f851411f0afdf152b6
Gareth <gareth.kh.lau@gmail.com>
2020-01-10 10:19:12 -0500

    + register favourites list component

--- client/src/routes/ProfileRoute.vue
+++ client/src/routes/ProfileRoute.vue
@@ -1,41 +1,33 @@
 <template>
     <h1>
         <section>
-            <b-tabs v-model="activeTab">
-                <b-tab-item label="Overview">
-                    
-                </b-tab-item>
+            <b-tabs v-model="activeTab" position="is-centered">
+                <b-tab-item label="Overview"> </b-tab-item>
 
                 <b-tab-item label="Favourites">
-                    
+                    <FavouritesList />
                 </b-tab-item>
 
-                <b-tab-item :visible="showBooks" label="Classes">
+                <b-tab-item label="Classes"> </b-tab-item>
 
-                </b-tab-item>
-
-                <b-tab-item label="Comments">
-    
-                </b-tab-item>
+                <b-tab-item label="Comments"> </b-tab-item>
             </b-tabs>
         </section>
     </h1>
 </template>
 
 <script>
-import { store } from '../store';
+import FavouritesList from '../components/profile/FavouritesList';
 
 export default {
     name: 'ProfileRoute',
+    components: { FavouritesList },
     data() {
         return {
-            storeState: store.state,
             activeTab: 0
         };
     },
-    methods: {},
-    beforeMount() {
-        console.log(this.storeState.user);
-    }
+
+    created() {}
 };
 </script>
